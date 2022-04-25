f325cc5d14aab7fcbc3209d45afecbb5d6bf760b
Midnight417 <69664426+Midnight417@users.noreply.github.com>
2022-04-24 19:18:49 -0700

    add setup

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -4,12 +4,22 @@ import GoogleProvider from "next-auth/providers/google"
 import DiscordProvider from "next-auth/providers/discord"
 
 export default NextAuth({
+  secret: process.env.SECRET,
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID!,
+      clientSecret: process.env.GOOGLE_SECRET!,
+    }),
+    DiscordProvider({
+      clientId: process.env.DISCORD_ID,
+      clientSecret: process.env.DISCORD_SECRET,
+    }),
+    // TODO: Add email providers
     // ...add more providers here
   ],
 })
